40aa28ac195b174620e03c4eb714f80d04945ad7
itay200303 <itay200303@gmail.com>
2025-02-05 18:13:58 +0200

    Add files via upload

---
+++ insert telev.sql
@@ -0,0 +1,5 @@
+INSERT INTO shop (number_catalog, brand, model, size_screen, resolution, price, quantity_stock, year_release, tv_smart, Os, panel_type)
+VALUES
+('1234ABC', 'Sony', 'QLED', 75, '8k', 25000.0, 10, 2025, 'Yes', 'Q-Symphony Pro', 'QLED');
+('5678ABC', 'LG', 'OLED', 65, '4K', 5000.00, 5, 2024, 'Yes', 'webOS', 'OLED');
+('9101DEF', 'Samsung', 'LED', 75, '8K', 9000.00, 3, 2023, 'Yes', 'Android TV', 'LED');
---
+++ televetion shop.sql
@@ -0,0 +1,16 @@
+CREATE TABLE shop(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+number_catalog TEXT UNIQUE,
+brand TEXT,
+model TEXT,
+size_screen INTEGER,
+resolution TEXT CHECK (resolution IN ('4k', '8k', 'Full HD')),
+price REAL,
+quantity_stock INTEGER,
+year_release INTEGER,
+tv_smart TEXT CHECK(tv_smart IN('Yes', 'No')),
+Os TEXT DEFAULT NULL,
+panel_type TEXT CHECK(panel_type IN('OLED', 'QLED', 'LED'))
+)
+
+
